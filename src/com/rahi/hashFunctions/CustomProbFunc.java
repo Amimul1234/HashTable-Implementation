3ce8c6b430921d20f6e3a89a17521ac035f313b3
Amimul Ehsan <1805056@ugrad.cse.buet.ac.bd>
2022-01-19 11:35:28 +0600

    Added corrected report.

--- src/com/rahi/hashFunctions/CustomProbFunc.java
+++ src/com/rahi/hashFunctions/CustomProbFunc.java
@@ -4,6 +4,7 @@ public class CustomProbFunc {
     public static int customHash( HashFunction hashFunction, String key, int i, int N ) {
         int C1 = 23;
         int C2 = 31;
-        return (hashFunction.hashValue(key, N) + C1 * i * AuxHash.auxHashGenerator(key, N) + C2 * i * i) % N;
+        int result = (hashFunction.hashValue(key, N) + C1 * i * AuxHash.auxHashGenerator(key, N) + C2 * i * i) % N;
+        return result < 0 ? result * -1 : result;
     }
 }
