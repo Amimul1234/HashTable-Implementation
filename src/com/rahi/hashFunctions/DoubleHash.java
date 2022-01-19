0bb139333b81bac5b3255dfa7039c5c4e199e500
Amimul Ehsan <1805056@ugrad.cse.buet.ac.bd>
2022-01-19 11:43:14 +0600

    Corrected probing number

--- src/com/rahi/hashFunctions/DoubleHash.java
+++ src/com/rahi/hashFunctions/DoubleHash.java
@@ -4,9 +4,6 @@ public class DoubleHash {
 
     public static int doubleHashGenerator( HashFunction hashFunction, String key, int i, int N ) {
         int index = (hashFunction.hashValue(key, N) + i * AuxHash.auxHashGenerator(key, N)) % N;
-
-        index = index < 0 ? index * -1 : index;
-
-        return index;
+        return index < 0 ? index * -1 : index;
     }
 }
